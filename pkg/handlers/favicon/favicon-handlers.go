25b373186d261cf0d3a7a0dc0603fa31a6e993e8
Murugappan Chetty <itsmurugappan@gmail.com>
2020-10-24 16:43:50 -0700

    Update favicon-handlers.go

--- pkg/handlers/favicon/favicon-handlers.go
+++ pkg/handlers/favicon/favicon-handlers.go
@@ -1,10 +1,12 @@
 package favicon
 
 import (
+	"os"
 	"net/http"
 )
 
 // have your image in /opt/images/fav.png
 func FaviconHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "/opt/images/fav.png")
+	koPath := os.Getenv("KO_DATA_PATH")
+	http.ServeFile(w, r, koPath+"/images/fav.png")
 }
